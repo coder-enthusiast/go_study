1c954a3c397234aab0d6154758025aa1e1e49355
zhuly406236000 <1048222543@qq.com>
2019-08-08 20:34:32 +0800

    计算字符串长度

--- pkg/day1/Test.go
+++ pkg/day1/Test.go
@@ -1,14 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main()  {
 	str:="abc朱志强"
+	//字节数
+	fmt.Println(len(str))
+	fmt.Println(utf8.RuneCountInString(str))
+	//字符数
 
+	fmt.Println()
 	for _,s:=range str  {
 		if(len(string(s))>=3){
 			fmt.Printf(string(s))
 		}
 	}
+
+	fmt.Println(str)
 
 }
