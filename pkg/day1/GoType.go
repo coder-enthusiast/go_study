5af2e0f2b573d871d8e49112e48fa3312c45053f
朱志强 <zhuzhiqiang@19lou.com>
2019-08-08 17:30:15 +0800

    变量声明

--- pkg/day1/GoType.go
+++ pkg/day1/GoType.go
@@ -5,6 +5,20 @@ import (
 	"reflect"
 )
 func main() {
+	fmt.Println("这是一个控制台输出函数 在fmt包下")
+
+	fmt.Println("变量声明")
+	var aa int =1
+	var bb float64=1.1
+	var cc bool=true
+	var dd string="golang"
+	fmt.Println(aa,bb,cc,dd)
+
+	fmt.Println("多变量声明")
+	var aaa, bbb, ccc int=1,2,3
+	fmt.Println(aaa,bbb,ccc)
+
+
 
 	fmt.Println("初始化默认值int float bool string")
 	var a int
