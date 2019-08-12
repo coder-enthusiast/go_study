889dca1d624dc2fae82aa83fcbcc2a9316a5f809
朱志强 <zhuzhiqiang@19lou.com>
2019-08-12 15:07:30 +0800

    select

--- src/pkg/day4/select.go
+++ src/pkg/day4/select.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	
+	ch := make(chan int,2)
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch:
+			fmt.Println(x)
+		case ch <- i:
+		}
+	}
+
+	time.Sleep(1000*time.Second)
 }
